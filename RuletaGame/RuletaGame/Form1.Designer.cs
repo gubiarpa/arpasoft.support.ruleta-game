bb84ea7b31306a5fd9140f43c40acb8cee3ab122
gubiarpa <billy.arredondo@pucp.pe>
2020-07-19 22:49:02 -0500

    Carrera de Ruleta
    Con esto, la ruleta ya corre.

--- RuletaGame/RuletaGame/Form1.Designer.cs
+++ RuletaGame/RuletaGame/Form1.Designer.cs
@@ -28,13 +28,302 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblCasilla01 = new System.Windows.Forms.Label();
+            this.lblCasilla16 = new System.Windows.Forms.Label();
+            this.lblCasilla15 = new System.Windows.Forms.Label();
+            this.lblCasilla12 = new System.Windows.Forms.Label();
+            this.lblCasilla13 = new System.Windows.Forms.Label();
+            this.lblCasilla14 = new System.Windows.Forms.Label();
+            this.lblCasilla11 = new System.Windows.Forms.Label();
+            this.lblCasilla02 = new System.Windows.Forms.Label();
+            this.lblCasilla03 = new System.Windows.Forms.Label();
+            this.lblCasilla10 = new System.Windows.Forms.Label();
+            this.lblCasilla04 = new System.Windows.Forms.Label();
+            this.lblCasilla09 = new System.Windows.Forms.Label();
+            this.lblCasilla05 = new System.Windows.Forms.Label();
+            this.lblCasilla06 = new System.Windows.Forms.Label();
+            this.lblCasilla07 = new System.Windows.Forms.Label();
+            this.lblCasilla08 = new System.Windows.Forms.Label();
+            this.btnEjecutarJug1 = new System.Windows.Forms.Button();
+            this.btnEjecutarJug2 = new System.Windows.Forms.Button();
+            this.lblPuntajeJug1 = new System.Windows.Forms.Label();
+            this.lblPuntajeJug2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCasilla01
+            // 
+            this.lblCasilla01.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla01.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla01.Location = new System.Drawing.Point(12, 9);
+            this.lblCasilla01.Name = "lblCasilla01";
+            this.lblCasilla01.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla01.TabIndex = 0;
+            this.lblCasilla01.Text = "10";
+            this.lblCasilla01.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCasilla16
+            // 
+            this.lblCasilla16.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla16.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla16.Location = new System.Drawing.Point(118, 9);
+            this.lblCasilla16.Name = "lblCasilla16";
+            this.lblCasilla16.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla16.TabIndex = 1;
+            this.lblCasilla16.Text = "100";
+            this.lblCasilla16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCasilla15
+            // 
+            this.lblCasilla15.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla15.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla15.Location = new System.Drawing.Point(224, 9);
+            this.lblCasilla15.Name = "lblCasilla15";
+            this.lblCasilla15.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla15.TabIndex = 2;
+            this.lblCasilla15.Text = "100";
+            this.lblCasilla15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCasilla12
+            // 
+            this.lblCasilla12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla12.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla12.Location = new System.Drawing.Point(542, 9);
+            this.lblCasilla12.Name = "lblCasilla12";
+            this.lblCasilla12.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla12.TabIndex = 5;
+            this.lblCasilla12.Text = "100";
+            this.lblCasilla12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCasilla13
+            // 
+            this.lblCasilla13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla13.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla13.Location = new System.Drawing.Point(436, 9);
+            this.lblCasilla13.Name = "lblCasilla13";
+            this.lblCasilla13.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla13.TabIndex = 4;
+            this.lblCasilla13.Text = "100";
+            this.lblCasilla13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCasilla14
+            // 
+            this.lblCasilla14.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla14.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla14.Location = new System.Drawing.Point(330, 9);
+            this.lblCasilla14.Name = "lblCasilla14";
+            this.lblCasilla14.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla14.TabIndex = 3;
+            this.lblCasilla14.Text = "100";
+            this.lblCasilla14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCasilla11
+            // 
+            this.lblCasilla11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla11.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla11.Location = new System.Drawing.Point(541, 115);
+            this.lblCasilla11.Name = "lblCasilla11";
+            this.lblCasilla11.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla11.TabIndex = 12;
+            this.lblCasilla11.Text = "100";
+            this.lblCasilla11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCasilla02
+            // 
+            this.lblCasilla02.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla02.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla02.Location = new System.Drawing.Point(12, 115);
+            this.lblCasilla02.Name = "lblCasilla02";
+            this.lblCasilla02.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla02.TabIndex = 13;
+            this.lblCasilla02.Text = "100";
+            this.lblCasilla02.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCasilla03
+            // 
+            this.lblCasilla03.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla03.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla03.Location = new System.Drawing.Point(12, 221);
+            this.lblCasilla03.Name = "lblCasilla03";
+            this.lblCasilla03.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla03.TabIndex = 15;
+            this.lblCasilla03.Text = "100";
+            this.lblCasilla03.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCasilla10
+            // 
+            this.lblCasilla10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla10.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla10.Location = new System.Drawing.Point(542, 221);
+            this.lblCasilla10.Name = "lblCasilla10";
+            this.lblCasilla10.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla10.TabIndex = 14;
+            this.lblCasilla10.Text = "100";
+            this.lblCasilla10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCasilla04
+            // 
+            this.lblCasilla04.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla04.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla04.Location = new System.Drawing.Point(12, 327);
+            this.lblCasilla04.Name = "lblCasilla04";
+            this.lblCasilla04.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla04.TabIndex = 17;
+            this.lblCasilla04.Text = "100";
+            this.lblCasilla04.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCasilla09
+            // 
+            this.lblCasilla09.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla09.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla09.Location = new System.Drawing.Point(542, 327);
+            this.lblCasilla09.Name = "lblCasilla09";
+            this.lblCasilla09.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla09.TabIndex = 16;
+            this.lblCasilla09.Text = "100";
+            this.lblCasilla09.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCasilla05
+            // 
+            this.lblCasilla05.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla05.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla05.Location = new System.Drawing.Point(118, 327);
+            this.lblCasilla05.Name = "lblCasilla05";
+            this.lblCasilla05.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla05.TabIndex = 1;
+            this.lblCasilla05.Text = "100";
+            this.lblCasilla05.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCasilla06
+            // 
+            this.lblCasilla06.BackColor = System.Drawing.Color.Transparent;
+            this.lblCasilla06.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla06.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla06.Location = new System.Drawing.Point(224, 327);
+            this.lblCasilla06.Name = "lblCasilla06";
+            this.lblCasilla06.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla06.TabIndex = 2;
+            this.lblCasilla06.Text = "100";
+            this.lblCasilla06.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCasilla07
+            // 
+            this.lblCasilla07.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla07.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla07.Location = new System.Drawing.Point(330, 327);
+            this.lblCasilla07.Name = "lblCasilla07";
+            this.lblCasilla07.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla07.TabIndex = 3;
+            this.lblCasilla07.Text = "100";
+            this.lblCasilla07.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCasilla08
+            // 
+            this.lblCasilla08.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCasilla08.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCasilla08.Location = new System.Drawing.Point(436, 327);
+            this.lblCasilla08.Name = "lblCasilla08";
+            this.lblCasilla08.Size = new System.Drawing.Size(100, 100);
+            this.lblCasilla08.TabIndex = 4;
+            this.lblCasilla08.Text = "100";
+            this.lblCasilla08.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnEjecutarJug1
+            // 
+            this.btnEjecutarJug1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEjecutarJug1.Location = new System.Drawing.Point(692, 80);
+            this.btnEjecutarJug1.Name = "btnEjecutarJug1";
+            this.btnEjecutarJug1.Size = new System.Drawing.Size(201, 48);
+            this.btnEjecutarJug1.TabIndex = 18;
+            this.btnEjecutarJug1.Text = "Jugador 1";
+            this.btnEjecutarJug1.UseVisualStyleBackColor = true;
+            this.btnEjecutarJug1.Click += new System.EventHandler(this.btnEjecutarJug1_Click);
+            // 
+            // btnEjecutarJug2
+            // 
+            this.btnEjecutarJug2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEjecutarJug2.Location = new System.Drawing.Point(692, 247);
+            this.btnEjecutarJug2.Name = "btnEjecutarJug2";
+            this.btnEjecutarJug2.Size = new System.Drawing.Size(201, 48);
+            this.btnEjecutarJug2.TabIndex = 19;
+            this.btnEjecutarJug2.Text = "Jugador 2";
+            this.btnEjecutarJug2.UseVisualStyleBackColor = true;
+            // 
+            // lblPuntajeJug1
+            // 
+            this.lblPuntajeJug1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuntajeJug1.Location = new System.Drawing.Point(689, 131);
+            this.lblPuntajeJug1.Name = "lblPuntajeJug1";
+            this.lblPuntajeJug1.Size = new System.Drawing.Size(204, 62);
+            this.lblPuntajeJug1.TabIndex = 20;
+            this.lblPuntajeJug1.Text = "0";
+            this.lblPuntajeJug1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPuntajeJug2
+            // 
+            this.lblPuntajeJug2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuntajeJug2.Location = new System.Drawing.Point(689, 298);
+            this.lblPuntajeJug2.Name = "lblPuntajeJug2";
+            this.lblPuntajeJug2.Size = new System.Drawing.Size(204, 62);
+            this.lblPuntajeJug2.TabIndex = 21;
+            this.lblPuntajeJug2.Text = "0";
+            this.lblPuntajeJug2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(919, 442);
+            this.Controls.Add(this.lblPuntajeJug2);
+            this.Controls.Add(this.lblPuntajeJug1);
+            this.Controls.Add(this.btnEjecutarJug2);
+            this.Controls.Add(this.btnEjecutarJug1);
+            this.Controls.Add(this.lblCasilla04);
+            this.Controls.Add(this.lblCasilla09);
+            this.Controls.Add(this.lblCasilla03);
+            this.Controls.Add(this.lblCasilla10);
+            this.Controls.Add(this.lblCasilla02);
+            this.Controls.Add(this.lblCasilla11);
+            this.Controls.Add(this.lblCasilla12);
+            this.Controls.Add(this.lblCasilla08);
+            this.Controls.Add(this.lblCasilla13);
+            this.Controls.Add(this.lblCasilla07);
+            this.Controls.Add(this.lblCasilla14);
+            this.Controls.Add(this.lblCasilla06);
+            this.Controls.Add(this.lblCasilla15);
+            this.Controls.Add(this.lblCasilla05);
+            this.Controls.Add(this.lblCasilla16);
+            this.Controls.Add(this.lblCasilla01);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCasilla01;
+        private System.Windows.Forms.Label lblCasilla16;
+        private System.Windows.Forms.Label lblCasilla15;
+        private System.Windows.Forms.Label lblCasilla12;
+        private System.Windows.Forms.Label lblCasilla13;
+        private System.Windows.Forms.Label lblCasilla14;
+        private System.Windows.Forms.Label lblCasilla11;
+        private System.Windows.Forms.Label lblCasilla02;
+        private System.Windows.Forms.Label lblCasilla03;
+        private System.Windows.Forms.Label lblCasilla10;
+        private System.Windows.Forms.Label lblCasilla04;
+        private System.Windows.Forms.Label lblCasilla09;
+        private System.Windows.Forms.Label lblCasilla05;
+        private System.Windows.Forms.Label lblCasilla06;
+        private System.Windows.Forms.Label lblCasilla07;
+        private System.Windows.Forms.Label lblCasilla08;
+        private System.Windows.Forms.Button btnEjecutarJug1;
+        private System.Windows.Forms.Button btnEjecutarJug2;
+        private System.Windows.Forms.Label lblPuntajeJug1;
+        private System.Windows.Forms.Label lblPuntajeJug2;
     }
 }
 
--- RuletaGame/RuletaGame/Form1.cs
+++ RuletaGame/RuletaGame/Form1.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,41 @@ namespace RuletaGame
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnEjecutarJug1_Click(object sender, EventArgs e)
+        {
+            int i = 1, veces = 10;
+            while (veces < 160)
+            {
+                lblCasilla01.BackColor = i == 1 ? Color.Aqua : Color.Transparent;
+                lblCasilla02.BackColor = i == 2 ? Color.Aqua : Color.Transparent;
+                lblCasilla03.BackColor = i == 3 ? Color.Aqua : Color.Transparent;
+                lblCasilla04.BackColor = i == 4 ? Color.Aqua : Color.Transparent;
+                lblCasilla05.BackColor = i == 5 ? Color.Aqua : Color.Transparent;
+                lblCasilla06.BackColor = i == 6 ? Color.Aqua : Color.Transparent;
+                lblCasilla07.BackColor = i == 7 ? Color.Aqua : Color.Transparent;
+                lblCasilla08.BackColor = i == 8 ? Color.Aqua : Color.Transparent;
+                lblCasilla09.BackColor = i == 9 ? Color.Aqua : Color.Transparent;
+                lblCasilla10.BackColor = i == 10 ? Color.Aqua : Color.Transparent;
+                lblCasilla11.BackColor = i == 11 ? Color.Aqua : Color.Transparent;
+                lblCasilla12.BackColor = i == 12 ? Color.Aqua : Color.Transparent;
+                lblCasilla13.BackColor = i == 13 ? Color.Aqua : Color.Transparent;
+                lblCasilla14.BackColor = i == 14 ? Color.Aqua : Color.Transparent;
+                lblCasilla15.BackColor = i == 15 ? Color.Aqua : Color.Transparent;
+                lblCasilla16.BackColor = i == 16 ? Color.Aqua : Color.Transparent;
+                Thread.Sleep(50);
+                if (i < 16)
+                {
+                    i++;
+                }
+                else
+                {
+                    i = 1;
+                }
+                veces++;
+                this.Refresh();
+            }
         }
     }
 }
